375d9e734ba11fc6999eb63e304ef6d67e96219c
alec <alecrodriguez1247@gmail.com>
2024-11-18 17:44:22 -0600

    adding backend capabilities

--- app/Main.hs
+++ app/Main.hs
@@ -7,12 +7,21 @@ import qualified Parser as Parser
 import qualified Evaluator as Evaluator
 import Text.Megaparsec (parse, eof)
 import Text.Megaparsec.Error 
-import Control.Applicative 
+import Control.Applicative ((<*))
 import qualified Network.Socket as NS
 import qualified Network.Socket.ByteString as NSB
 import qualified Network.TLS as TLS
+import qualified Network.TLS.Extra.Cipher as Cipher
+import Data.X509.CertificateStore 
 import qualified Data.ByteString as BS
+import Data.Word 
+import qualified Data.List.NonEmpty as NE
+import Data.Char (ord)
 
+
+
+stringToWord8 :: String -> [Word8]
+stringToWord8 = map (fromIntegral . ord)
 
 main :: IO ()
 main = do
