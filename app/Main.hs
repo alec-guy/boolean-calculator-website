c9159575796b9760764bafcfe14c4d63ed24ad16
alec <alecrodriguez1247@gmail.com>
2024-11-18 18:45:55 -0600

    fixed lots of stuff

--- app/Main.hs
+++ app/Main.hs
@@ -14,7 +14,7 @@ import qualified Network.Socket as NS
 import qualified Network.Socket.ByteString as NSB
 import qualified Network.TLS as TLS
 import qualified Network.TLS.Extra.Cipher as Cipher
-import qualified Network.TLS.Quic as Quic
+import qualified Network.TLS.QUIC as Quic
 import Data.X509.CertificateStore 
 import qualified Data.ByteString as BS
 import Data.Word 
@@ -30,30 +30,27 @@ main :: IO ()
 main = do
   putStrLn "Hello Haskell!"
   maybeCertificateStore <- readCertificateStore "C:/Users/alecb/certsTwo/cacert.pem"
-  maybeCredential       <- credentialLoadX509 "C:/Users/alecb/logicCalcPrivateKey/public.pem" "C:/Users/alecb/logicCalcPrivateKey/private.pem"
+  maybeCredential       <- TLS.credentialLoadX509 "C:/Users/alecb/logicCalcPrivateKey/public.pem" "C:/Users/alecb/logicCalcPrivateKey/private.pem"
   store                 <- (case maybeCertificateStore of
                              Nothing     -> error "could not find certificate store"
-                            Just (store) ->  return store)
+                             Just (store) ->  return store)
   credential            <- (case maybeCredential of 
                              Left s -> error s 
                              Right c -> return c)
-  addr                  <-  NE.head <$> NS.getAddrInfo (Just NS.defaultHints) (Just hostname) (Just "8000")
+  addr                  <-  NE.head <$> NS.getAddrInfo (Just NS.defaultHints) (Just "www.localhost.com") (Just "8000")
   let socketAddress = NS.addrAddress addr
-      port8000      <- NS.openSocket addr
-      socketType    <- NS.getSocketType port8000
+  port8000      <- NS.openSocket addr
+  socketType    <- NS.getSocketType port8000
   case NS.isSupportedSocketType socketType  of 
       True  -> putStrLn "successful socket type"
       False -> do putStrLn $ show socketType 
                   error "Not supported"
   NS.connect port8000 socketAddress
-  let myBackend      = MyBackend{mySocket = port8000}
-      newShared      = (serverShared defaultParamsServer) {sharedCAStore = store}
-      newShared'     = newShared $
-                      {sharedCredentials = Credentials [credential]
-                      ,sharedHelloExtensions = 
-                      }  
-      myParamsServer = defaultParamsServer {serverShared = newShared}
-  context <- contextNew myBackend defaultParamsServer
+  let myBackend      = Types.MyBackend {Types.mySockey = port8000}
+      newShared      = (TLS.serverShared TLS.defaultParamsServer) {TLS.sharedCAStore = store}
+      newShared'     = newShared {TLS.sharedCredentials = TLS.Credentials [credential] ,TLS.sharedHelloExtensions = []}  
+      myParamsServer = TLS.defaultParamsServer {TLS.serverShared = newShared'}
+  context <- TLS.contextNew myBackend myParamsServer
   putStrLn "Testing Expression parser."
   i <- getLine 
   case parse (Parser.parseExpression <* eof)  "" i  of 
@@ -61,6 +58,7 @@ main = do
     (Right expr) -> do 
                   putStrLn "Successful parse"
                   putStrLn $ show $ Evaluator.evalBExpr expr
+  TLS.bye context
 
 
   
